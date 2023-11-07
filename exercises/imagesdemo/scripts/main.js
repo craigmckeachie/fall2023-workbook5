1f1829b1f92c0fcb5bb290ad66040dcac633619d
Craig McKeachie <craigdmckeachie@gmail.com>
2023-11-07 10:31:02 -0500

    update

--- exercises/imagesdemo/scripts/main.js
+++ exercises/imagesdemo/scripts/main.js
@@ -4,12 +4,12 @@
 // const decorateButton = document.getElementById("decorateButton");
 const decorateButton = document.querySelector("#decorateButton");
 
-
 //functions
 function decorate() {
-    console.log("garland, garland,...");
+  const paragraphs = document.querySelectorAll("p");
+  const p = paragraphs[0];
+  p.style.backgroundColor = "aqua";
 }
 
 //wire-up connect functions to events
 decorateButton.onclick = decorate;
-
