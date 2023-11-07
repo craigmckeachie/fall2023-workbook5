a9d2304434411d50d4639f9c22d6541abd7957e5
Craig McKeachie <craigdmckeachie@gmail.com>
2023-11-07 14:02:45 -0500

    add options to list

--- exercises/dynamicimages/scripts/main.js
+++ exercises/dynamicimages/scripts/main.js
@@ -2,10 +2,10 @@
 
 //data
 let imageFiles = [
-  { path: "https://placehold.co/300x300", description: "placeholder" },
-  { path: "https://placehold.co/300x300", description: "placeholder" },
-  { path: "https://placehold.co/300x300", description: "placeholder" },
-  { path: "https://placehold.co/300x300", description: "placeholder" },
+  { path: "https://placehold.co/300x300", description: "placeholder 1" },
+  { path: "https://placehold.co/300x300", description: "placeholder 2" },
+  { path: "https://placehold.co/300x300", description: "placeholder 3" },
+  { path: "https://placehold.co/300x300", description: "placeholder 4" },
   { path: "https://placehold.co/300x300", description: "placeholder" },
   { path: "https://placehold.co/300x300", description: "placeholder" },
   { path: "https://placehold.co/300x300", description: "placeholder" },
@@ -27,7 +27,10 @@ console.log(imageList, addImageButton, clearImagesButton, imagesDiv);
 //functions
 function loadImageList() {
     for (const imageFile of imageFiles) {
-        console.log(imageFile);
+        let option = document.createElement("option");
+        option.value = imageFile.path;
+        option.innerText = imageFile.description;
+        imageList.appendChild(option);
     }
 }
 
