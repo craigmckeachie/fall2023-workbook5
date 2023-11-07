52d667540fb366e72385f02744ac044a5a0d94e7
Craig McKeachie <craigdmckeachie@gmail.com>
2023-11-07 14:56:39 -0500

    event handler for add image

--- exercises/dynamicimages/scripts/main.js
+++ exercises/dynamicimages/scripts/main.js
@@ -5,14 +5,13 @@ let imageFiles = [
   { id: "1", path: "https://placehold.co/300x300", description: "placeholder 1" },
   { id: "2", path: "https://placehold.co/300x300", description: "placeholder 2" },
   { id: "3", path: "https://placehold.co/300x300", description: "placeholder 3" },
-  { id: "4", path: "https://placehold.co/300x300", description: "placeholder 5" },
-  { id: "5", path: "https://placehold.co/300x300", description: "placeholder 6" },
-  { id: "6", path: "https://placehold.co/300x300", description: "placeholder 7" },
-  { id: "7", path: "https://placehold.co/300x300", description: "placeholder 8" },
-  { id: "8", path: "https://placehold.co/300x300", description: "placeholder 9" },
-  { id: "9", path: "https://placehold.co/300x300", description: "placeholder 10" },
-  { id: "10", path: "https://placehold.co/300x300", description: "placeholder 11" },
-  { id: "11", path: "https://placehold.co/300x300", description: "placeholder 12" },
+  { id: "4", path: "https://placehold.co/300x300", description: "placeholder 4" },
+  { id: "5", path: "https://placehold.co/300x300", description: "placeholder 5" },
+  { id: "6", path: "https://placehold.co/300x300", description: "placeholder 6" },
+  { id: "7", path: "https://placehold.co/300x300", description: "placeholder 7" },
+  { id: "8", path: "https://placehold.co/300x300", description: "placeholder 8" },
+  { id: "9", path: "https://placehold.co/300x300", description: "placeholder 9" },
+  { id: "10", path: "https://placehold.co/300x300", description: "placeholder 10" },
 ];
 
 //get elements
@@ -33,6 +32,11 @@ function loadImageList() {
   }
 }
 
-loadImageList();
+function addImage() {
+  console.log("working");
+}
 
 //wire-up function
+addImageButton.onclick = addImage;
+
+loadImageList();
