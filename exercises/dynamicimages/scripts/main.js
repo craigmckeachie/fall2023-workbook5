e53f249ba9d75230a7a47679ecee4630f97d6f27
Craig McKeachie <craigdmckeachie@gmail.com>
2023-11-08 09:50:16 -0500

    remove images

--- exercises/dynamicimages/scripts/main.js
+++ exercises/dynamicimages/scripts/main.js
@@ -1,6 +1,8 @@
 "use strict";
 
 //data
+// variable imageFiles gets declared and assigned an array []
+// which contains objects representing an image file
 let imageFiles = [
   { id: "1", path: "https://placehold.co/300x300", description: "placeholder 1" },
   { id: "2", path: "https://placehold.co/300x300", description: "placeholder 2" },
@@ -15,7 +17,10 @@ let imageFiles = [
 ];
 
 //get elements
+//variable gets declared and assigned an html element select list
 const imageList = document.querySelector("#imageList");
+
+//putting the button into variable from the html
 const addImageButton = document.querySelector("#addImageButton");
 const clearImagesButton = document.querySelector("#clearImagesButton");
 const imagesDiv = document.querySelector("#imagesDiv");
@@ -23,7 +28,9 @@ const imagesDiv = document.querySelector("#imagesDiv");
 console.log(imageList, addImageButton, clearImagesButton, imagesDiv);
 
 //functions
+// creating a function named load images
 function loadImageList() {
+  //creating for of loop, creating variable imageFile to get each item (object) in the array of imageFiles 
   for (const imageFile of imageFiles) {
     let option = document.createElement("option");
     option.value = imageFile.id;
@@ -41,14 +48,19 @@ function addImage() {
       //set a src attribute on the image to the imageFile's path
       image.src = imageFile.path;
       //set the alt attribute on the image to the imageFile's description
-      image.alt = imageFile.description
+      image.alt = imageFile.description;
       //add image to div
-      imagesDiv.appendChild(image)
+      imagesDiv.appendChild(image);
     }
   }
 }
 
+function removeImages() {
+    imagesDiv.innerHTML = "";
+}
+
 //wire-up function
 addImageButton.onclick = addImage;
+clearImagesButton.onclick = removeImages;
 
 loadImageList();
