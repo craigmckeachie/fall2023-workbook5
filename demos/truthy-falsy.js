449224544b14d5e5ce4b6459589a44797552fc60
Craig McKeachie <craigdmckeachie@gmail.com>
2023-11-09 11:26:11 -0500

    truthy falsy

---
+++ demos/truthy-falsy.js
@@ -0,0 +1,14 @@
+"use strict";
+
+let value;
+value = "";
+value= undefined;
+value = null;
+
+if (value == "" || value == undefined || value == null) {
+  console.log(false);
+}
+
+if(!value){
+ console.log(false);
+}
