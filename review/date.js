2d4d1f62e2f869fd6b8f5ad56b6553f40d5d26fe
Craig McKeachie <craigdmckeachie@gmail.com>
2023-11-20 09:27:34 -0500

    date

--- review/date.js
+++ review/date.js
@@ -1,3 +1,6 @@
 let date = new Date();
-console.log(date.getFullYear());
+console.log((date.getMonth() + 1) + "/" + date.getDate() +  "/" + date.getFullYear());
 "11/20/2023"
+
+console.log(date.getDay());// day of the week, Sunday is 0, Monday is 1
+console.log(date.getDate());// date of the month 1-31
