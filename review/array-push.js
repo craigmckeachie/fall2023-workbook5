b19fb2a13f6411ff70c9b804512e831c170dbde5
Craig McKeachie <craigdmckeachie@gmail.com>
2023-11-20 11:25:34 -0500

    array push

---
+++ review/array-push.js
@@ -0,0 +1,22 @@
+let numbers = [];
+console.log(numbers);
+numbers.push(1);
+console.log(numbers);
+numbers.push(2);
+console.log(numbers);
+numbers.push(3);
+console.log(numbers);
+
+let students = [];
+console.log(students);
+
+students.push("Salin");
+console.log(students);
+
+students.push("Brad");
+console.log(students);
+
+students.push("Sarita");
+console.log(students);
+
+
