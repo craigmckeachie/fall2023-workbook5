1c2ba47cb71a39287cc1a55bed26394270e15b10
Craig McKeachie <craigdmckeachie@gmail.com>
2023-11-20 09:58:48 -0500

    parseInt

---
+++ review/escaping-strings.js
@@ -0,0 +1,2 @@
+let sentence = 'This  is "great"!';
+console.log(sentence);
---
+++ review/order-of-operations.js
@@ -0,0 +1,6 @@
+// The acronym PEMDAS is common in the United States and France.
+// It stands for Parentheses, Exponents, Multiplication/Division, Addition/Subtraction.
+
+let result = 3 + 4 * 9;
+console.log(result);
+
