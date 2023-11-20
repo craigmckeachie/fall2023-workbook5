ebbfa6fae10f636278d28e330622ab7852e18c2b
Craig McKeachie <craigdmckeachie@gmail.com>
2023-11-20 10:34:43 -0500

    switch fallthrough

---
+++ review/switch-fallthrough.js
@@ -0,0 +1,30 @@
+let date = new Date();
+let month = date.getMonth() + 1;
+let quarter = 0;
+
+switch (month) {
+  case 1:
+  case 2:
+  case 3:
+    quarter = 1;
+    break;
+  case 4:
+  case 5:
+  case 6:
+    quarter = 2;
+    break;
+  case 7:
+  case 8:
+  case 9:
+    quarter = 3;
+    break;
+  case 10:
+  case 11:
+  case 12:
+    quarter = 4;
+    break;
+  default:
+    break;
+}
+
+console.log(quarter);
