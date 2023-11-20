c4604a1e12922db172dd954864ee80b18c39ec9e
Craig McKeachie <craigdmckeachie@gmail.com>
2023-11-20 10:42:20 -0500

    if comparison

---
+++ review/if-comparison.js
@@ -0,0 +1,12 @@
+let message = "fail"; // assignment
+
+if (message == "fail") {
+  //comparision equal
+  console.log("failed");
+}
+
+message = "pass";
+if (message != "fail") {
+  //comparison not equal
+  console.log("succeeded");
+}
