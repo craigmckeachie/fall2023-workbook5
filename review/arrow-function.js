8f505fc611b770fe26843b4fdf1935488b845755
Craig McKeachie <craigdmckeachie@gmail.com>
2023-11-20 15:22:46 -0500

    add function

--- review/arrow-function.js
+++ review/arrow-function.js
@@ -14,10 +14,35 @@ console.log(result2);
 
 
 
-function add(a,b) {
-  return a+ b;
-}
+// function add(a,b) {
+//   return a+ b;
+// }
+
+
+
+//keyword name parameter list
+// const name = parameter list arrow =>
+
+ const add = (a,b)=> a+ b;
+
 
 console.log(add(2, 2));
+
+const functionName = (params) => "what you want to return";
+
+
+function buildTableRow(park){
+  let tr = parksTableBody.insertRow();
+
+  let td1 = tr.insertCell();
+  td1.innerText = park.LocationName;
+
+  let td2 = tr.insertCell();
+  td2.innerText = park.State;
+
+  let td3 = tr.insertCell();
+  td3.innerText = park.Phone;
+}
+
 
 
