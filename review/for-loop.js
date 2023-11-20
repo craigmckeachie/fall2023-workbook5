c6aa85c788da5574a87fd56223f670fb0b5ff470
Craig McKeachie <craigdmckeachie@gmail.com>
2023-11-20 10:50:00 -0500

    for loop

---
+++ review/for-loop.js
@@ -0,0 +1,12 @@
+let colors = ["red", "green", "blue", "purple", "yellow", "orange"];
+
+// for (const color of colors) {
+//     console.log(color);
+// }
+
+
+// console.log(colors[3]);
+for (let index = 0; index < colors.length; index++) {
+    const color = colors[index];
+    console.log(color);  
+}
