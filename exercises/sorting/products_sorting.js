bb40eeab0beebc64b0df6dfd6eb7705b0a1cda39
Craig McKeachie <craigdmckeachie@gmail.com>
2023-11-21 15:12:07 -0500

    sort start

---
+++ exercises/sorting/products_sorting.js
@@ -0,0 +1,7 @@
+"use strict";
+
+let products = [
+  { prodId: 2, name: "Notepads (12 pk)", price: 12.29 },
+  { prodId: 12, name: "Black Pens (12 pk)", price: 5.7 },
+  { prodId: 22, name: "Stapler", price: 12.79 },
+];
