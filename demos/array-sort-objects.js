8b455ae9ddd88821c3acfd72f4ad68732f59da21
Craig McKeachie <craigdmckeachie@gmail.com>
2023-11-21 14:35:02 -0500

    array sort object 2

--- demos/array-sort-objects.js
+++ demos/array-sort-objects.js
@@ -1,5 +1,15 @@
 let products = [
-  { prodId: 2, item: "Notepads (12 pk)", price: 12.29 },
-  { prodId: 12, item: "Black Pens (12 pk)", price: 5.7 },
-  { prodId: 22, item: "Stapler", price: 12.79 },
+  { prodId: 2, name: "Notepads (12 pk)", price: 12.29 },
+  { prodId: 12, name: "Black Pens (12 pk)", price: 5.7 },
+  { prodId: 22, name: "Stapler", price: 12.79 },
 ];
+
+
+
+products.sort((productA, productB)=> {
+    if(productA.name < productB.name) return -1
+    else if(productA.name == productB.name) return 0
+    else return 1; 
+});
+
+console.log(products);
