a895c502a9eb1859cb5cdf18cc8cf7931577de2a
Craig McKeachie <craigdmckeachie@gmail.com>
2023-11-21 13:58:56 -0500

    array sort numbers 2

--- demos/array-sort-numbers.js
+++ demos/array-sort-numbers.js
@@ -1,10 +1,30 @@
 let numbers = [100, 20, 55, 67, 11, 2, 88];
 
+// version 1
+// function compare(a, b) {
+//   if (a < b) return -1; // right order
+//   else if (a == b) return 0;
+//   else return 1; // swap, wrong order
+// }
+
+//version 2
 function compare(a, b) {
-  if (a < b) return -1; // right order
-  else if (a == b) return 0;
-  else return 1; // swap, wrong order
+  return a - b;
 }
 
-numbers.sort(compare);
+// numbers.sort(compare);
+
+//version 3
+// numbers.sort(function (a, b) {
+//   return a - b;
+// });
+
+//version 4
+// numbers.sort((a, b) => {
+//   return a - b;
+// });
+
+//version 5
+numbers.sort((a, b) => a - b);
+
 console.log(numbers);
