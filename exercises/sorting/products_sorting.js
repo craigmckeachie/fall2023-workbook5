8f9772575edc6b90d69d8696dc80e767afd4fbed
Craig McKeachie <craigdmckeachie@gmail.com>
2023-11-22 09:03:50 -0500

    desc sort

--- exercises/sorting/products_sorting.js
+++ exercises/sorting/products_sorting.js
@@ -4,9 +4,8 @@ let products = [
   { prodId: 2, name: "Notepads (12 pk)", price: 12.29 },
   { prodId: 12, name: "Black Pens (12 pk)", price: 5.7 },
   { prodId: 50, name: "Printer", price: 199.99 },
-  { prodId: 22, name: "Lenova Laptop", price: 500.00 },
+  { prodId: 22, name: "Lenova Laptop", price: 500.0 },
 ];
-
 
 products.sort((productA, productB) => {
   if (productA.name < productB.name) return -1;
@@ -19,7 +18,7 @@ console.log(products);
 
 console.log("----------------------------------------");
 
-products.sort((productA, productB) => Number(productA.price) - Number(productB.price));
+products.sort((productA, productB) => Number(productB.price) - Number(productA.price));
 
 console.log("Sorted by Fee");
 console.log(products);
